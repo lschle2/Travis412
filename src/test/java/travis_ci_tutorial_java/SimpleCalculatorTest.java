84b812e06728d43064084b409d5e8fcfddbf2cd6
lschle2 <79662658+lschle2@users.noreply.github.com>
2021-02-25 16:26:33 -0500

    Additional Assert Tests

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -23,9 +23,30 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.divide(15, 3), 5);
 	}
-	public void testAdd1() {
+	public void testTrue() {
 		SimpleCalculator calc = new SimpleCalculator();
 		int x = calc.add(3, 3);
 		assertTrue(x == 6);
+	}
+	public void testFalse() {
+		SimpleCalculator calc = new SimpleCalculator();
+		int z = calc.minus(5, 3);
+		assertFalse(z == 5);
+	}
+	public void testNull() {
+		SimpleCalculator calc = new SimpleCalculator();
+		int n = calc.minus(5, 3);
+		assertNotNull(n);
+	}
+	public void testSame() {
+		SimpleCalculator calc = new SimpleCalculator();
+		int s = calc.minus(5, 3);
+		assertSame(s, s);
+	}
+	public void testNotSame() {
+		SimpleCalculator calc = new SimpleCalculator();
+		int d = calc.minus(5, 3);
+		int g = calc.add(3, 3);
+		assertSame(d, g);
 	}
 }
